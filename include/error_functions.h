5bd3d0afb24921842b93b362b8fd1f0e9b95cab0
Ehab Saleh <ehab.saleh@lrz.de>
2025-12-27 18:28:51 +0100

    Update error_functions.h, error_functions.c and tlpi_hdr.h

--- include/error_functions.h
+++ include/error_functions.h
@@ -2,9 +2,6 @@
 #define ERROR_FUNCTIONS_H
 void errMsg(const char *format, ...);
 #ifdef __GNUC__
-/* This macro stops 'gcc -Wall' complaining that "control reaches
-end of non-void function" if we use the following functions to
-terminate main() or some other non-void function. */
 #define NORETURN __attribute__ ((__noreturn__))
 #else
 #define NORETURN
