2e037d4787a2006c6e0095a6524716688bed2065
Ehab Saleh <ehab.saleh@lrz.de>
2025-12-27 19:05:58 +0100

    Add get_num.h/c

---
+++ include/get_num.h
@@ -0,0 +1,14 @@
+#ifndef GET_NUM_H
+#define GET_NUM_H
+#define GN_NONNEG 
+#define GN_GT_0 01 02 /* Value must be >= 0 */
+/* Value must be > 0 */
+#define GN_ANY_BASE 
+#define GN_BASE_8 
+#define GN_BASE_16 0100 0200 0400 /* By default, integers are decimal */
+/* Can use any base - like strtol(3) */
+/* Value is expressed in octal */
+/* Value is expressed in hexadecimal */
+long getLong(const char *arg, int flags, const char *name);
+int getInt(const char *arg, int flags, const char *name);
+#endif
