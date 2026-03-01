74a41acd170bf5ec379efae55a9bb6671e1a7536
Ehab Saleh <ehab.saleh@lrz.de>
2026-03-01 10:55:23 +0100

    common headers

---
+++ include/mem_bench.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef MEM_BENCH_H
+#define MEM_BENCH_H
+
+#include<time.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdint.h>
+#include<stddef.h>
+#include<limits.h>
+#include<math.h>
+#include<fcntl.h> //provide control over file descriptors
+                  //
+#endif
