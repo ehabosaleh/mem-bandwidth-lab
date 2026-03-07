05d4a8852ebf91221dd145e3275d6fb44477a2a9
Ehab Saleh <ehab.saleh@lrz.de>
2026-03-07 09:23:45 +0100

    declare parent/child queues library functions

--- include/mq_bench.h
+++ include/mq_bench.h
@@ -8,4 +8,14 @@
 mqd_t mq_p2c;
 mqd_t mq_c2p;
 
+void send_all(mqd_t mq, const void *buf, size_t size);
+void recv_all(mqd_t mq, void *buf, size_t size);
+void usage(const char *argv0);
+size_t parse_size(const char* s);
+void child_loop(void);
+double measure_latency_one(size_t msg_size, int iters,int warmup);
+void mq_setup(mqd_t *mq,size_t max_msg);
+void mq_cleanup(mqd_t *mq);
+
+
 #endif
