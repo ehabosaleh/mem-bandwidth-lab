298d33e896405f78fc4e06e25781323897726691
Ehab Saleh <ehab.saleh@lrz.de>
2026-03-08 10:20:56 +0100

    update mq_bench.h

--- include/mq_bench.h
+++ include/mq_bench.h
@@ -3,19 +3,22 @@
 
 #include"mem_bench.h"
 
-#define p2c "mq_parent_to_child"
-#define c2p "mq_child_to_parent"
+#define P2C "mq_parent_to_child"
+#define C2P "mq_child_to_parent"
 mqd_t mq_p2c;
 mqd_t mq_c2p;
 
 void send_all(mqd_t mq, const void *buf, size_t size);
 void recv_all(mqd_t mq, void *buf, size_t size);
+
 void usage(const char *argv0);
 size_t parse_size(const char* s);
+
 void child_loop(void);
 double measure_latency_one(size_t msg_size, int iters,int warmup);
-void mq_setup(mqd_t *mq,size_t max_msg);
-void mq_cleanup(mqd_t *mq);
+
+void mq_setup(size_t max_msg);
+void mq_cleanup(void);
 
 
 #endif
