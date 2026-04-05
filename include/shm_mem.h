0a4bc9982ffa84f57b980fd279e92f1201aac87d
root <ehab.saleh@lrz.de>
2026-04-05 20:07:13 +0200

    update main header of shm_mem

--- include/shm_mem.h
+++ include/shm_mem.h
@@ -1,6 +1,16 @@
 #include <sys/mman.h>
 #include"mem_bench.h"
 
+#define SHM_NAME "/shm_bench"
+
+typedef struct{
+	volatile int ready;
+    	volatile int done;
+    	size_t size;
+    	char *buffer;
+} shm_region;
+
+
 void usage(const char *argv0);
 size_t parse_size(const char* s);
-
+void pin_cpu(int cpu);
