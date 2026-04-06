2c495c2fb4946d57fccf36429980eb82d38b2467
root <ehab.saleh@lrz.de>
2026-04-06 11:46:59 +0200

    add main source for shm_mem benchmark

---
+++ tests/shm_mem_main.c
@@ -0,0 +1,56 @@
+#include "../include/shm_mem_bench.h"
+
+int main(int argc, char **argv){
+	size_t min_bytes = 1;
+    	size_t max_bytes = 1024ULL*1024ULL;
+    	int iters = 1000;
+    	int warmup = 100;
+	int writer_core=0,reader_core=1;
+	bool is_writer=1;
+
+    	for (int i=1;i<argc;i++) {
+        	if(strncmp(argv[i], "--min-bytes=", 12) == 0) min_bytes = parse_size(argv[i] + 12);
+        	else if(strncmp(argv[i], "--max-bytes=", 12) == 0) max_bytes = parse_size(argv[i] + 12);
+        	else if(strncmp(argv[i], "--iters=", 8) == 0) iters = atoi(argv[i]+8);
+        	else if(strncmp(argv[i], "--warmup=", 9) == 0) warmup = atoi(argv[i]+9);
+		else if(strncmp(argv[i],"--reader_core=", 11)==0) reader_core=atoi(argv[i]+11);
+		else if(strncmp(argv[i],"--writer_core=", 11)==0) writer_core=atoi(argv[i]+11);
+        	else if(strncmp(argv[i],"--is_writer=",12 )==0) is_writer=atoi(argv[i]+12);
+		else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
+            		usage(argv[0]);
+        	}else{
+            		fprintf(stderr, "Unknown arg: %s\n", argv[i]);
+            		usage(argv[0]);
+        	}
+    	}
+	
+	if(!is_writer){
+		shm_region *shm;
+		void *addr=shm_init(is_writer, &shm);
+		run_reader(shm,reader_core);
+		shm_cleanup(is_writer,shm, addr,sizeof(shm_region)+MAX_SIZE);
+        	return 0;
+	}
+	
+    	shm_region *shm;
+    	void *addr = shm_init(is_writer, &shm);
+	pid_t pid;
+	char* reader_core_arg=NULL;
+	snprintf(reader_core_arg, 16, "--reader_core=%d",reader_core);
+
+	pid=fork();
+	if(pid<0){
+		perror("fork():");
+		exit(1);
+	}
+	if(pid==0){
+		execl("./shm_mem_bench","--is_writer=0",reader_core_arg,NULL);
+		perror("child–execl");
+		exit(1);
+	}
+	
+        run_writer(shm,writer_core,warmup,iters);
+    	shm_cleanup(is_writer,shm,addr,sizeof(shm_region)+MAX_SIZE);
+    	return 0;
+}
+
