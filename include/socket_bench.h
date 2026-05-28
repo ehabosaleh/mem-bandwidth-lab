73fd81eff204e25b2c0edcf2686c4f256124dbae
Ehab Saleh <ehab.saleh@lrz.de>
2026-05-28 13:34:48 +0200

    refactor socket_bench.h

--- include/socket_bench.h
+++ include/socket_bench.h
@@ -18,13 +18,13 @@ typedef struct {
 }socket_struct_t;
 
 
-inline int validate_type(int type){
+static inline int validate_type(int type){
 	if(type!=SOCK_STREAM&&type!=SOCK_DGRAM&&type!=SOCK_SEQPACKET)
 		return 1;
 	else
 		return 0;
 }
-inline int validate_domain(int domain){
+static inline int validate_domain(int domain){
 	if(domain!=AF_UNIX&&domain!=AF_INET&&domain!=AF_INET6)
 		return 1;
 	else
@@ -36,10 +36,12 @@ int unix_addr_init(const char*path, struct sockaddr_un*addr);
 int unix_server_init( socket_struct_t *socket,const char*path,const int domain,const int type);
 int unix_client_init( socket_struct_t *socket,const char*path,const int domain,const int type);
 
-ssize_t unix_read_all(int fd,char*buffer,size_t size);
-ssize_t unix_write_all(int fd, const char*buffer,size_t size);
+ssize_t unix_read_all(socket_struct_t*s,char*buffer,size_t size);
+ssize_t unix_write_all(socket_struct_t*s, const char*buffer,size_t size);
 
 
 void socket_cleanup(socket_struct_t *socket);
 
+int parse_string_to_type(const char* s);
+int parse_string_to_domain(const char* s);
 #endif
