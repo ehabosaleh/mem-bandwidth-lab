9224395df68522104df6ba75e1e30bc00e8f539b
ehabosaleh <ehab.saleh@lrz.de>
2026-08-22 15:33:31 +0200

    add usage and parse_size body

--- src/dma_bench.c
+++ src/dma_bench.c
@@ -1,6 +1,45 @@
 #include"../include/dma_bench.h"
 #include"../include/socket_bench.h"
 
+void usage(const char* prog_name){
+    fprintf(stderr,
+           "Usage: %s [--min-bytes=N] [--max-bytes=N] [--iters=N] [--warmup=N] [--rdma-op=write/read]\n"
+           "Examples:\n"
+           "  %s --min-bytes=1 --max-bytes=4MiB --iters=1000 --warmup=100 --rdma-op=write\n"
+           "  %s --min-bytes=1 --max-bytes=4MiB --iters=1000 --warmup=100 --rdma-op=read\n",
+           prog_name, prog_name, prog_name);
+    exit(EXIT_FAILURE);
+}
+
+size_t parse_size(const char* s){
+	char*end=NULL;
+      	errno=0;
+      	unsigned long long v=strtoull(s,&end,10);
+      	if (errno!=0 || end==s){
+			fprintf(stderr, "Invalid size: %s\n", s);
+           	exit(EXIT_FAILURE);
+		}
+      	unsigned long long mul=1;
+      	if(*end){
+		if(strcasecmp(end,"k")==0) mul=1000ULL;
+          	else if(strcasecmp(end,"m")==0) mul=1000ULL*1000ULL;
+          	else if(strcasecmp(end,"g")==0) mul=1000ULL*1000ULL*1000ULL;
+          	else if(strcasecmp(end,"kib")==0) mul=1024ULL;
+          	else if(strcasecmp(end,"mib")==0) mul=1024ULL*1024ULL;
+          	else if(strcasecmp(end,"gib")==0) mul=1024ULL*1024ULL*1024ULL;
+		else{
+               		fprintf(stderr, "Unknown size suffix: %s\n", end);
+               		exit(EXIT_FAILURE);
+           	}
+      }
+
+      if(mul!=0&&v>(unsigned long)SIZE_MAX/mul){
+              fprintf(stderr,"Passed size is too large\n");
+              exit(EXIT_FAILURE);
+      }
+
+      return (size_t)v*mul;
+  }
 
 struct rdma_resource *rdma_resource_init(size_t size){
     struct rdma_resource *res=NULL;
