4be4301ebe6ca4daea0e1ce298af47d2bb4a062b
ehabosaleh <ehab.saleh@lrz.de>
2026-08-22 14:47:33 +0200

    add struct to rdma_connection_info

--- include/dma_bench.h
+++ include/dma_bench.h
@@ -29,7 +29,7 @@ struct rdma_connection_info{
 
 struct rdma_resource* rdma_resource_init(size_t size); // reset state
 int rdma_qp_to_initial(struct rdma_resource* res,uint8_t port_num);// init state
-int rdma_qp_to_rtr(struct rdma_resource* res,const rdma_connection_info *local,const rdma_connection_info *remote,uint8_t port_num);// ready to receive state
+int rdma_qp_to_rtr(struct rdma_resource* res,const struct rdma_connection_info *local,const struct rdma_connection_info *remote,uint8_t port_num);// ready to receive state
 int rdma_qp_to_rts(struct rdma_resource* res,uint32_t local_psn);// ready to send state
 int rdma_get_local_info(struct rdma_resource* res, struct rdma_connection_info* info, uint8_t port_num);
 
@@ -38,8 +38,8 @@ int rdma_resource_cleanup(struct rdma_resource* res);
 
 int rdma_check_completion(struct rdma_resource* res);
 
-int rdma_exchange_info_server(struct rdma_resource*res,rdma_connection_info*local_info,rdma_connection_info*remote_info);
-int rdma_exchange_info_client(struct rdma_resource* res, rdma_connection_info* local_info,rdma_connection_info* remote_info);
+int rdma_exchange_info_server(struct rdma_resource*res, struct rdma_connection_info*local_info, struct rdma_connection_info*remote_info);
+int rdma_exchange_info_client(struct rdma_resource* res, struct rdma_connection_info* local_info, struct rdma_connection_info* remote_info);
 
 int rdma_write(struct rdma_resource* res, void* buf, size_t size, uint64_t remote_addr, uint32_t rkey);
 int rdma_read(struct rdma_resource* res, void* buf, size_t size, uint64_t remote_addr, uint32_t rkey);
