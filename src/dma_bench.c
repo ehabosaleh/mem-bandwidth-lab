a282624590aad7e8f384dc4af545c80413a5e0f6
ehabosaleh <ehab.saleh@lrz.de>
2026-08-22 10:40:17 +0200

    change rdma_exchange_info signatures

---
+++ src/dma_bench.c
@@ -0,0 +1,344 @@
+#include"../include/dma_bench.h"
+#include"../include/socket_bench.h"
+
+
+struct rdma_resource *rdma_resource_init(size_t size){
+    struct rdma_resource *res=NULL;
+    struct ibv_device **device_list=NULL;
+    int num_devices=0;
+
+    if(size==0){
+        errno=EINVAL;
+        return NULL;
+    }
+
+    res=calloc(1,sizeof(*res));
+    if (!res){
+        perror("calloc rdma_resource");
+        return NULL;
+    }
+
+    device_list=ibv_get_device_list(&num_devices);
+    if (!device_list){
+        perror("ibv_get_device_list");
+        goto error;
+    }
+
+    if (num_devices==0){
+        fprintf(stderr, "No RDMA devices were found\n");
+        goto error;
+    }
+
+    res->ctx = ibv_open_device(device_list[0]);
+    if(!res->ctx){
+        perror("ibv_open_device");
+        goto error;
+    }
+
+    ibv_free_device_list(device_list);
+    device_list = NULL;
+
+    res->pd = ibv_alloc_pd(res->ctx);
+    if(!res->pd){
+        perror("ibv_alloc_pd");
+        goto error;
+    }
+    res->buffer=calloc(1, size);
+    if(!res->buffer){
+        perror("calloc RDMA buffer");
+        goto error;
+    }
+
+    res->buffer_size=size;
+    res->mr=ibv_reg_mr(res->pd,res->buffer,size,IBV_ACCESS_LOCAL_WRITE|IBV_ACCESS_REMOTE_READ|IBV_ACCESS_REMOTE_WRITE);
+
+    if(!res->mr){
+        perror("ibv_reg_mr");
+        goto error;
+    }
+
+    res->cq=ibv_create_cq(res->ctx,1,NULL,NULL,0);
+
+    if (!res->cq){
+        perror("ibv_create_cq");
+        goto error;
+    }
+
+    struct ibv_qp_init_attr qp_attr = {0};
+
+    qp_attr.qp_type=IBV_QPT_RC;
+    qp_attr.send_cq=res->cq;
+    qp_attr.recv_cq=res->cq;
+
+    qp_attr.cap.max_send_wr=1;
+    qp_attr.cap.max_recv_wr=1;
+    qp_attr.cap.max_send_sge=1;
+    qp_attr.cap.max_recv_sge=1;
+
+    qp_attr.sq_sig_all=0;
+
+    res->qp = ibv_create_qp(res->pd, &qp_attr);
+    if (!res->qp){
+        perror("ibv_create_qp");
+        goto error;
+    }
+
+    return res;
+
+error:
+    if (device_list)
+        ibv_free_device_list(device_list);
+    if (res) {
+        if (res->qp)
+            ibv_destroy_qp(res->qp);
+        if (res->cq)
+            ibv_destroy_cq(res->cq);
+        if (res->mr)
+            ibv_dereg_mr(res->mr);
+
+        free(res->buffer);
+        if (res->pd)
+            ibv_dealloc_pd(res->pd);
+        if (res->ctx)
+            ibv_close_device(res->ctx);
+        free(res);
+    }
+    return NULL;
+}
+
+int rdma_qp_to_initial(struct rdma_resource* res,uint8_t port_num){
+    struct ibv_qp_attr attr = {0};
+    int flags=0;
+    int ret=1;
+
+    attr.qp_state = IBV_QPS_INIT;
+    attr.port_num = port_num;
+    attr.pkey_index = 0;
+    attr.qp_access_flags = IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE;
+
+    flags = IBV_QP_STATE | IBV_QP_PKEY_INDEX | IBV_QP_PORT | IBV_QP_ACCESS_FLAGS;
+
+    ret = ibv_modify_qp(res->qp, &attr, flags);
+    if(ret) {
+        perror("ibv_modify_qp to INIT");
+        return -1;
+    }
+    return 0;
+}
+
+int rdma_qp_to_rtr(struct rdma_resource* res, uint32_t remote_qpn, uint16_t dlid, uint8_t port_num){
+    struct ibv_qp_attr attr = {0};
+    int flags=0;
+    int ret=1;
+
+    attr.qp_state = IBV_QPS_RTR;
+    attr.path_mtu = IBV_MTU_1024;
+    attr.dest_qp_num = remote_qpn;
+    attr.rq_psn = 0;
+    attr.max_dest_rd_atomic = 1;
+    attr.min_rnr_timer = 12;
+    attr.ah_attr.is_global = 0;
+    attr.ah_attr.dlid = dlid;
+    attr.ah_attr.sl = 0;
+    attr.ah_attr.src_path_bits = 0;
+    attr.ah_attr.port_num = port_num;
+
+    flags = IBV_QP_STATE | IBV_QP_AV | IBV_QP_PATH_MTU | IBV_QP_DEST_QPN |
+            IBV_QP_RQ_PSN | IBV_QP_MAX_DEST_RD_ATOMIC | IBV_QP_MIN_RNR_TIMER;
+
+    ret = ibv_modify_qp(res->qp, &attr, flags);
+    if(ret) {
+        perror("ibv_modify_qp to RTR");
+        return -1;
+    }
+    return 0;
+}
+int rdma_qp_to_rts(struct rdma_resource* res){
+    struct ibv_qp_attr attr = {0};
+    int flags=0;
+    int ret=1;
+
+    attr.qp_state = IBV_QPS_RTS;
+    attr.timeout = 14;
+    attr.retry_cnt = 7;
+    attr.rnr_retry = 7;
+    attr.sq_psn = 0;
+    attr.max_rd_atomic = 1;
+
+    flags = IBV_QP_STATE | IBV_QP_TIMEOUT | IBV_QP_RETRY_CNT |
+            IBV_QP_RNR_RETRY | IBV_QP_SQ_PSN | IBV_QP_MAX_QP_RD_ATOMIC;
+
+    ret = ibv_modify_qp(res->qp, &attr, flags);
+    if(ret) {
+        perror("ibv_modify_qp to RTS");
+        return -1;
+    }
+    return 0;
+}
+int rdma_get_local_info(struct rdma_resource* res, struct rdma_connection_info* info, uint8_t port_num){
+    if(!res || !info){
+        errno=EINVAL;
+        return -1;
+    }
+    struct ibv_port_attr port_attr;
+    if(ibv_query_port(res->ctx, port_num, &port_attr)){
+        perror("ibv_query_port");
+        return -1;
+    }
+    info->qp_num = res->qp->qp_num;
+    info->psn = 0;
+    info->rkey = res->mr->rkey;
+    info->buffer_addr = (uint64_t)(uintptr_t)res->mr->addr;
+    info->lid = port_attr.lid;
+    info->mtu = port_attr.active_mtu;
+
+    return 0;
+}
+
+int rdma_exchange_info_server(struct rdma_resource*res,rdma_connection_info*local_info,rdma_connection_info*remote_info){
+    if(!res||!local_info||!remote_info){
+        errno=EINVAL;
+        return -1;
+    }
+    socket_struct_t socket;
+    if(inet_server_init(&socket,NULL,12345,AF_INET,SOCK_STREAM)!=0){
+        fprintf(stderr,"Failed to initialize server socket\n");
+        return -1;
+    }
+    if(write_all(&socket,(char*)local_info,sizeof(*local_info))!=sizeof(*local_info)){
+        fprintf(stderr,"Failed to send local connection info\n");
+        socket_cleanup(&socket);
+        return -1;
+    }
+   
+    if(read_all(&socket,(char*)remote_info,sizeof(*remote_info))!=sizeof(*remote_info)){
+        fprintf(stderr,"Failed to receive remote connection info\n");
+        socket_cleanup(&socket);
+        return -1;
+    }
+    socket_cleanup(&socket);
+    if(rdma_qp_to_rtr(res,remote_info->qp_num,remote_info->lid,1)!=0){
+        fprintf(stderr,"Failed to transition QP to RTR\n");
+        return -1;
+    }
+    if(rdma_qp_to_rts(res)!=0){
+        fprintf(stderr,"Failed to transition QP to RTS\n");
+        return -1;
+    }
+    return 0;
+}
+
+int rdma_exchange_info_client(struct rdma_resource* res, rdma_connection_info* local_info, rdma_connection_info* remote_info){
+    if(!res||!local_info||!remote_info){
+        errno=EINVAL;
+        return -1;
+    }
+    socket_struct_t socket;
+    if(inet_client_init(&socket,NULL,12345,AF_INET,SOCK_STREAM)!=0){
+        fprintf(stderr,"Failed to initialize client socket\n");
+        return -1;
+    }
+    if(read_all(&socket,(char*)remote_info,sizeof(*remote_info))!=sizeof(*remote_info)){
+        fprintf(stderr,"Failed to receive remote connection info\n");
+        socket_cleanup(&socket);
+        return -1;
+    }
+    if(write_all(&socket,(char*)local_info,sizeof(*local_info))!=sizeof(*local_info)){
+        fprintf(stderr,"Failed to send local connection info\n");
+        socket_cleanup(&socket);
+        return -1;
+    }
+    socket_cleanup(&socket);
+    if(rdma_qp_to_rtr(res,remote_info->qp_num,remote_info->lid,1)!=0){
+        fprintf(stderr,"Failed to transition QP to RTR\n");
+        return -1;
+    }
+    if(rdma_qp_to_rts(res)!=0){
+        fprintf(stderr,"Failed to transition QP to RTS\n");
+        return -1;
+    }
+    return 0;
+}
+
+
+
+
+int rdma_write(struct rdma_resource* res, void* buf, size_t size, uint64_t remote_addr, uint32_t rkey) {
+    struct ibv_sge sge = {0};
+    sge.addr = (uintptr_t)buf;
+    sge.length = size;
+    sge.lkey = res->mr->lkey;
+
+    struct ibv_send_wr wr = {0};
+    wr.wr_id = 1;
+    wr.opcode = IBV_WR_RDMA_WRITE;
+    wr.sg_list = &sge;
+    wr.num_sge = 1;
+    wr.send_flags = IBV_SEND_SIGNALED;
+    wr.wr.rdma.remote_addr = remote_addr;
+    wr.wr.rdma.rkey = rkey;
+
+    struct ibv_send_wr *bad_wr;
+    if (ibv_post_send(res->qp, &wr, &bad_wr)) {
+        perror("ibv_post_send");
+        return -1;
+    }
+    return 0;
+}
+int rdma_read(struct rdma_resource* res, void* buf, size_t size, uint64_t remote_addr, uint32_t rkey) {
+    struct ibv_sge sge = {0};
+    sge.addr = (uintptr_t)buf;
+    sge.length = size;
+    sge.lkey = res->mr->lkey;
+
+    struct ibv_send_wr wr = {0};
+    wr.wr_id = 1;
+    wr.opcode = IBV_WR_RDMA_READ;
+    wr.sg_list = &sge;
+    wr.num_sge = 1;
+    wr.send_flags = IBV_SEND_SIGNALED;
+    wr.wr.rdma.remote_addr = remote_addr;
+    wr.wr.rdma.rkey = rkey;
+
+    struct ibv_send_wr *bad_wr;
+    if (ibv_post_send(res->qp, &wr, &bad_wr)) {
+        perror("ibv_post_send");
+        return -1;
+    }
+
+    return 0;
+}
+
+int rdma_check_completion(struct rdma_resource* res) {
+    struct ibv_wc wc;
+    int num_completions = ibv_poll_cq(res->cq, 1, &wc);
+    if (num_completions < 0) {
+        perror("ibv_poll_cq");
+        return -1;
+    } else if (num_completions == 0) {
+        return 0; 
+    } else {
+        if (wc.status != IBV_WC_SUCCESS) {
+            fprintf(stderr, "RDMA operation failed: %s\n", ibv_wc_status_str(wc.status));
+            return -1;
+        }
+        return 1;
+    }
+}
+
+int rdma_resource_cleanup(struct rdma_resource* res){
+    if(!res)
+        return -1;
+    if(res->qp)
+        ibv_destroy_qp(res->qp);
+    if(res->cq)
+        ibv_destroy_cq(res->cq);
+    if(res->mr)
+        ibv_dereg_mr(res->mr);
+    if(res->pd)
+        ibv_dealloc_pd(res->pd);
+    if(res->ctx)
+        ibv_close_device(res->ctx);
+    free(res);
+    return 0;
+}
