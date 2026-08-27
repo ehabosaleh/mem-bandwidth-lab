1a946221d442103232c137b37021f94114775a46
ehabosaleh <ehab.saleh@lrz.de>
2026-08-27 06:39:50 +0200

    rename sources to match swapped file descriptors

--- tests/fifo_client.c
+++ tests/fifo_client.c
@@ -1,74 +1,50 @@
 #include"../include/fifo_bench.h"
 
-int main(int argc,char**argv){
-    size_t min_bytes = 1;
-    size_t max_bytes = 1024ULL*1024ULL;
-    int iters = 1000;
-    int warmup = 100;
-
-    for (int i=1;i<argc;i++) {
-         if (strncmp(argv[i], "--min-bytes=", 12) == 0) min_bytes = parse_size(argv[i] + 12);
-         else if (strncmp(argv[i], "--max-bytes=", 12) == 0) max_bytes = parse_size(argv[i] + 12);
-         else if (strncmp(argv[i], "--iters=", 8) == 0) iters = atoi(argv[i] + 8);
-         else if (strncmp(argv[i], "--warmup=", 9) == 0) warmup = atoi(argv[i] + 9);
-         else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
-             usage(argv[0]);
-         } else {
-             fprintf(stderr, "Unknown arg: %s\n", argv[i]);
-             usage(argv[0]);
-         }
-     }
-
-    int rfd=open(SERVER_FIFO,O_WRONLY);
-    int wfd=open(CLIENT_FIFO,O_RDONLY);
-    if(wfd<0||rfd<0){
-        perror("Client-FIFO:Open");
+int main(){
+    /*
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+    printf("CWD = %s\n", cwd);
+    printf("Creating: %s\n", SERVER_FIFO);
+    */
+    
+    if(mkfifo(SERVER_FIFO,0666)<0 && errno!=EEXIST){
+        perror("Server: mkfifo()");
         exit(EXIT_FAILURE);
     }
-    uint64_t temp_wsize=1;
-    uint64_t temp_rsize=1;
-    char*temp_buf=(char*)malloc(temp_wsize);
-
-    for (int i=0;i<warmup;i++) {
-        write_all(wfd,&temp_wsize,sizeof(temp_wsize));
-        write_all(wfd,temp_buf,temp_wsize);
-        
-        read_all(rfd,&temp_rsize,sizeof(temp_rsize));
-        read_all(rfd,temp_buf,temp_rsize);
+    if(mkfifo(CLIENT_FIFO,0666)<0 && errno!=EEXIST){
+         perror("Server: mkfifo()");
+         exit(EXIT_FAILURE);
+     }
+    
+    int wfd=open(SERVER_FIFO,O_WRONLY);
+    int rfd=open(CLIENT_FIFO,O_RDONLY);
+    if(rfd<0||wfd<0){
+        perror("CLIENT-FIFO:Open");
+        exit(EXIT_FAILURE);
     }
 
-
-    printf("%-12s%-12s%-12s \n","Bytes","Latency(us)","Bandwidth(MB/s)");
-    double t1=0.0;
-    for(uint64_t size=min_bytes;size<max_bytes;size*=2){
+    for(;;){
+        uint64_t size;
+        read_all(rfd,&size,sizeof(size));
+        if(size==0)
+            break;
         char*buf=malloc(size);
-        for(int i=0;i<size;i++)
-            *(buf+i)=(char)i;
-        
-        for(int i=0;i<iters;i++){  
-            double t0=now_sec();
-            
-            write_all(wfd,&size,sizeof(size));
-            write_all(wfd,buf,size);
+        read_all(rfd,buf,size);
+        if(strcasecmp(buf,"FIN")==0)
+            break;
 
-            read_all(rfd,&size,sizeof(size));
-            read_all(rfd,buf,size);
-            
-            t1+=now_sec()-t0;
-        }
-        double rtt=t1/iters;
-        double one_way=rtt/2.0;
-        
-        printf("%-12lu %-12.3f %-12.3f\n",size, one_way*1e6, (size/one_way)/(1024.0*1024.0));
-        free(buf);
+
+        write_all(wfd,&size,sizeof(size));
+        write_all(wfd,buf,size);
+
+        free(buf); 
+
     }
-    char *terminate="FIN";
-    uint64_t size=3;
-    write_all(wfd,&size,sizeof(size));
-    write_all(wfd,terminate,size);
 
     close(rfd);
     close(wfd);
+    unlink(SERVER_FIFO);
+    unlink(CLIENT_FIFO);
 
-    return 0;
 }
