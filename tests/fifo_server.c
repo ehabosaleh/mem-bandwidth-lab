63564a5576db20d2a332bf3b22280e661b7f1aab
ehabosaleh <ehab.saleh@lrz.de>
2026-08-27 07:13:01 +0200

    add server file descriptor

--- tests/fifo_server.c
+++ tests/fifo_server.c
@@ -19,10 +19,10 @@ int main(int argc,char**argv){
          }
      }
 
-    int rfd=open(SERVER_FIFO,O_WRONLY);
-    int wfd=open(CLIENT_FIFO,O_RDONLY);
+    int rfd=open(SERVER_FIFO,O_RDONLY);
+    int wfd=open(CLIENT_FIFO,O_WRONLY);
     if(wfd<0||rfd<0){
-        perror("Client-FIFO:Open");
+        perror("Server-FIFO:Open");
         exit(EXIT_FAILURE);
     }
     uint64_t temp_wsize=1;
